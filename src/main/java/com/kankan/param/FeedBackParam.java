932775e6f048c64fe5a113c9dd7083b87a54eeab
qiding <qiding@syswin.com>
2021-02-09 21:33:10 +0800

    aaa

--- src/main/java/com/kankan/param/FeedBackParam.java
+++ src/main/java/com/kankan/param/FeedBackParam.java
@@ -14,14 +14,16 @@ import java.util.List;
  */
 @Data
 public class FeedBackParam {
-    private Long userId;
-    private String feedback;
-    private List<String> pictures;
-    public Feedback toFeedBack(String username) {
-       return Feedback.builder()
-         .userId(userId)
-         .feedback(feedback)
-         .createTime(Instant.now().toEpochMilli())
-         .build();
-    }
+  private Long userId;
+  private String feedback;
+  private List<String> pictures;
+
+  public Feedback toFeedBack(String username) {
+    return Feedback.builder()
+      .userId(userId)
+      .feedback(feedback)
+      .pictures(this.pictures)
+      .createTime(Instant.now().toEpochMilli())
+      .build();
+  }
 }
