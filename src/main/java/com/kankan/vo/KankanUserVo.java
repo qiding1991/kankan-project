11ca7a149b6e7f554e13623497b8c6bb857d8e7b
qiding <qiding@syswin.com>
2021-06-22 23:19:30 +0800

    aa

--- src/main/java/com/kankan/vo/KankanUserVo.java
+++ src/main/java/com/kankan/vo/KankanUserVo.java
@@ -4,17 +4,17 @@ import com.kankan.vo.tab.TabItemVo;
 import lombok.Data;
 
 /**
- * @author <qiding@qiding.com>
- * Created on 2020-12-08
+ * @author <qiding@qiding.com> Created on 2020-12-08
  */
 @Data
 public class KankanUserVo extends TabItemVo {
-    private String userName;
-    private String userId;
-    private String remark;
-    private String picture;
-    private Long fansCount;
-    private Long followCount;
-    private Long readCount;
-    private boolean followStatus;
+
+  private String userName;
+  private String userId;
+  private String remark;
+  private String picture;
+  private Long fansCount = 0L;
+  private Long followCount = 0L;
+  private Long readCount = 0L;
+  private boolean followStatus;
 }
